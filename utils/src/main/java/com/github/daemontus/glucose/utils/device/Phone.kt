8b3210c0e91d9b44ebc1f612ff26823ff5f7ecb1
Samuel Pastva <daemontus@gmail.com>
2016-06-16 12:23:41 +0200

    Add Phone tests

--- utils/src/main/java/com/github/daemontus/glucose/utils/device/Phone.kt
+++ utils/src/main/java/com/github/daemontus/glucose/utils/device/Phone.kt
@@ -1,13 +1,22 @@
 package com.github.daemontus.glucose.utils.device
 
+import android.Manifest
 import android.content.Context
+import android.support.annotation.RequiresPermission
 import android.telephony.TelephonyManager
 
 object Phone {
 
+    /**
+     * Attempts to get the phone number of this device.
+     * Please be aware that the phone number might not be available due to the configuration
+     * of the SIM card / other reasons. However, there is no better solution, because sometimes
+     * the device just doesn't know the number. In those cases, the method returns empty string.
+     */
+    @RequiresPermission(Manifest.permission.READ_PHONE_STATE)
     fun getPrimaryPhoneNumber(ctx: Context): String {
         val manager = ctx.getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
-        return manager.line1Number
+        return manager.line1Number ?: ""
     }
 
 }
