3bfe6fb390b14762bf26ad0a8fa1d3de03796edc
Samuel Pastva <daemontus@gmail.com>
2016-08-18 17:54:54 +0200

    better docs

--- utils/src/main/java/com/glucose/app/ContextAccessors.kt
+++ utils/src/main/java/com/glucose/app/ContextAccessors.kt
@@ -7,6 +7,8 @@ import kotlin.reflect.KProperty
 
 /**
  * A shortcut to access the [Presenter] activity assuming it has a specific type.
+ *
+ * @see [ParentActivity]
  */
 fun <A: Activity> Presenter.getActivity(): A {
     @Suppress("UNCHECKED_CAST") //this is not on us
@@ -24,6 +26,8 @@ class ParentActivity<out A: Activity> : ReadOnlyProperty<Presenter, A> {
 
 /**
  * A shortcut to access the [Presenter] application assuming it has a specific type.
+ *
+ * @see [ParentApp]
  */
 fun <A: Application> Presenter.getApp(): A {
     @Suppress("UNCHECKED_CAST")
