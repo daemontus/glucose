91fcd5548b23c0b7b324bc73f6666eed50b0e971
Samuel Pastva <daemontus@gmail.com>
2017-04-30 16:33:33 +0200

    lifecycle host

--- core/src/main/java/com/glucose2/app/component/Component.kt
+++ core/src/main/java/com/glucose2/app/component/Component.kt
@@ -20,7 +20,8 @@ open class Component : DataHost, LifecycleHost {
     override final val isDestroyed get() = state == State.DESTROYED
 
     override final val isAlive get() = state >= State.ALIVE
-    override final val alive: ObservableBinder = ObservableBinder()
+    // we are already alive
+    override final val alive: ObservableBinder = ObservableBinder().apply { this.performStart() }
 
     override final val isAttached get() = state >= State.ATTACHED
     override final val attached: ObservableBinder = ObservableBinder()
@@ -30,6 +31,84 @@ open class Component : DataHost, LifecycleHost {
 
     override final val isResumed get() = state >= State.RESUMED
     override final val resumed: ObservableBinder = ObservableBinder()
+
+    private inline fun assertLifecycleChange(
+            from: State, to: State, transition: () -> Unit
+    ) {
+        if (state != from) throw LifecycleException("Unexpected transition. Expected $from -> $to. Got $state -> $to instead.")
+        transition()
+        if (state != to) throw LifecycleException("Unexpected transition. Expected $from -> $to. Got $state -> $to instead. Maybe forgot to call super?")
+    }
+
+    internal fun performAttach() {
+        assertLifecycleChange(State.ALIVE, State.ATTACHED) { onAttach() }
+    }
+
+    internal fun performStart() {
+        assertLifecycleChange(State.ATTACHED, State.STARTED) { onStart() }
+    }
+
+    internal fun performResume() {
+        assertLifecycleChange(State.STARTED, State.RESUMED) { onResume() }
+    }
+
+    internal fun performPause() {
+        assertLifecycleChange(State.RESUMED, State.STARTED) { onPause() }
+    }
+
+    internal fun performStop() {
+        assertLifecycleChange(State.STARTED, State.ATTACHED) { onStop() }
+    }
+
+    internal fun performDetach() {
+        assertLifecycleChange(State.ATTACHED, State.ALIVE) { onDetach() }
+    }
+
+    internal fun performDestroy() {
+        assertLifecycleChange(State.ALIVE, State.DESTROYED) { onDestroy() }
+    }
+
+    @CallSuper
+    protected open fun onAttach() {
+        state = State.ATTACHED
+        attached.performStart()
+    }
+
+    @CallSuper
+    protected open fun onStart() {
+        state = State.STARTED
+        started.performStart()
+    }
+
+    @CallSuper
+    protected open fun onResume() {
+        state = State.RESUMED
+        resumed.performStart()
+    }
+
+    @CallSuper
+    protected open fun onPause() {
+        resumed.performStop()
+        state = State.STARTED
+    }
+
+    @CallSuper
+    protected open fun onStop() {
+        started.performStop()
+        state = State.ATTACHED
+    }
+
+    @CallSuper
+    protected open fun onDetach() {
+        attached.performStop()
+        state = State.ALIVE
+    }
+
+    @CallSuper
+    protected open fun onDestroy() {
+        alive.performStop()
+        state = State.DESTROYED
+    }
 
     // ========== DataHost interface ==========
 
@@ -42,7 +121,7 @@ open class Component : DataHost, LifecycleHost {
 
     override final val dataBound: ObservableBinder = ObservableBinder()
 
-    override fun bindData(data: Bundle): Bundle? {
+    override final fun bindData(data: Bundle): Bundle? {
         val old = _data
         onDataBind(data)
         if (_data !== data) {
@@ -51,7 +130,7 @@ open class Component : DataHost, LifecycleHost {
         return old
     }
 
-    override fun resetData(): Bundle? {
+    override final fun resetData(): Bundle? {
         val old = _data
         onDataReset()
         if (isBound) {
--- core/src/main/java/com/glucose2/app/component/LifecycleHost.kt
+++ core/src/main/java/com/glucose2/app/component/LifecycleHost.kt
@@ -2,20 +2,70 @@ package com.glucose2.app.component
 
 import com.glucose2.rx.ObservableBinder
 
+/**
+ * Abstraction of types which provide access to the lifecycle similar to the one of an Activity.
+ * By default, the LifecycleHost should mirror the underlying Activity. However,
+ * not all components have to strictly follow this. For example, you can have an adapter
+ * that will stop items that aren't currently visible but not detach them.
+ *
+ * However, under no circumstances should the state of the Activity be "smaller"
+ * than that of the LifecycleHost (can't have a resumed LifecycleHost in a
+ * stopped Activity for example). This implies that all lifecycle changes
+ * should be synchronous and performed on main thread. So when the onPause
+ * of the underlying activity shouldn't return unless all associated lifecycle hosts
+ * are paused.
+ *
+ * Also note that the state indicators work as a hierarchy. That is, whenever [isStarted] is true,
+ * [isAttached] must be also true, since you can't start a component which isn't attached and so on.
+ *
+ * The implementations of this interface will usually provide methods such as
+ * onAttach, onStart or onResume so that you can react to the lifecycle changes.
+ */
 interface LifecycleHost {
 
+    /**
+     * Indicates that this host is destroyed and can't be used again.
+     */
     val isDestroyed: Boolean
 
+    /**
+     * Indicates that this host hasn't been destroyed yet.
+     */
     val isAlive: Boolean
+
+    /**
+     * [ObservableBinder] monitoring the changes of the [isAlive] state.
+     */
     val alive: ObservableBinder
 
+    /**
+     * Indicates that this host has been attached to the main hierarchy.
+     */
     val isAttached: Boolean
+
+    /**
+     * [ObservableBinder] monitoring the changes of the [isAttached] state.
+     */
     val attached: ObservableBinder
 
+    /**
+     * Indicates that the host is part of a started Activity and should be started too.
+     */
     val isStarted: Boolean
+
+    /**
+     * [ObservableBinder] monitoring the changes of the [isStarted] state.
+     */
     val started: ObservableBinder
 
+    /**
+     * Indicates that the host is part of a resumed Activity and should be resumed too.
+     */
     val isResumed: Boolean
+
+    /**
+     * [ObservableBinder] monitoring the changes of the [isResumed] state.
+     */
     val resumed: ObservableBinder
 
 }
