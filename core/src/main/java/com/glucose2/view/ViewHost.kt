3f77e202c353f52ddf4697c62d0e4baf55140218
Samuel Pastva <daemontus@gmail.com>
2017-04-30 14:27:05 +0200

    some more clean up

--- core/src/main/java/com/glucose2/view/ViewHost.kt
+++ core/src/main/java/com/glucose2/view/ViewHost.kt
@@ -2,8 +2,14 @@ package com.glucose2.view
 
 import android.view.View
 
+/**
+ * A simple interface which unites classes that are associated with views.
+ */
 interface ViewHost {
 
+    /**
+     * The view object associated with this host.
+     */
     val view: View
 
 }
--- core/src/main/java/com/glucose2/view/package.kt
+++ core/src/main/java/com/glucose2/view/package.kt
@@ -2,10 +2,37 @@ package com.glucose2.view
 
 import android.support.annotation.IdRes
 import android.view.View
-import com.glucose.app.Presenter
+import android.view.ViewGroup
 
-inline fun <reified V: View> Presenter.findView(@IdRes viewId: Int): V
+/**
+ * Find the view with given ID in this hosts view tree.
+ * If no such view is present, [KotlinNullPointerException] will be thrown.
+ */
+inline fun <reified V: View> ViewHost.findView(@IdRes viewId: Int): V
         = view.findViewById(viewId) as V
 
-inline fun <reified V: View> Presenter.findOptionalView(@IdRes viewId: Int): V?
+/**
+ * Find an optional view with given ID in this hosts tree.
+ * If no such view is present, null is returned.
+ */
+inline fun <reified V: View> ViewHost.findOptionalView(@IdRes viewId: Int): V?
         = view.findViewById(viewId) as V?
+
+/**
+ * Traverse the view tree of this host. The ordering of the traversal is
+ * determined by the tree structure.
+ *
+ * You can switch between two traversal modes: preOrder and postOrder.
+ */
+fun ViewHost.viewTraversal(postOrder: Boolean = true): Sequence<View> = traverseView(this.view)
+
+private fun traverseView(view: View, postOrder: Boolean): Sequence<View> {
+    return if (view !is ViewGroup) {
+        sequenceOf(view)
+    } else {
+        val children = (0 until view.childCount).asSequence().flatMap { index ->
+            traverseView(view.getChildAt(index), postOrder)
+        }
+        if (postOrder) children + sequenceOf(view) else sequenceOf(view) + children
+    }
+}
